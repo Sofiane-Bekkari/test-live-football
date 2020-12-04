8b47bd3a1b35fef8bbf884523a78219b3df62f55
root <root@localhost.localdomain>
2020-12-04 16:03:25 +0000

    Added drop mune to small size

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // get a date for current year
 const date = document.getElementById("date");
-date.innerHTML = new Date().getFullYear(); 
+date.innerHTML = new Date().getFullYear();
+
+
+// show and hide navbar items
+const burger = document.querySelector('.burger');
+const navBar = document.querySelector('.navbar');
+
+burger.addEventListener('click', function(){
+    navBar.classList.toggle('show-nav');
+} );
