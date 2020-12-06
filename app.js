622180602534b04caa1f6f6921b25028b7666390
root <root@localhost.localdomain>
2020-12-06 17:59:05 +0000

    Added functionality for dropdown menu

--- app.js
+++ app.js
@@ -13,3 +13,28 @@ burger.addEventListener('click', function(){
 
 
 // add functionality to dropdown menu
+const btnChevron = document.querySelectorAll('.fa-chevron-down');
+const listItems = document.querySelector('.list-items');
+const listContent = document.querySelectorAll('.dropdown-content');
+//console.log(btnChevron);
+//console.log(listItems);
+//console.log(listContent);
+
+listItems.addEventListener('click', function(e){
+    const id = e.target.dataset.id
+    //console.log(e.target.dataset.id);
+    if (id) {
+        // if already id exsist remove active 
+        listContent.forEach((content) => {
+                content.classList.remove('active');
+            });
+
+        // add active for specific id 
+        const element = document.getElementById(id);
+                console.log(element)
+                element.classList.add("active");
+
+        }
+
+});
+
